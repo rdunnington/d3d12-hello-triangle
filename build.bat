fbce7676e1823a0f801d2be846bf478a91332b0e
Reuben Dunnington <reuben.dunnington@gmail.com>
2022-02-22 22:19:28 -0800

    build.bat: remove link warning, add env error message

--- build.bat
+++ build.bat
@@ -1,24 +1,14 @@
 @ECHO OFF
 
-REM Don't want to keep adding stuff to PATH, otherwise the batch file will start failing after too many times
 IF NOT DEFINED DevEnvDir (
-	CALL "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
-)
-
-FOR /F "tokens=* USEBACKQ" %%F IN (`uuidgen`) DO (
-	SET GUID=%%F
+	ECHO Run this batch file from a x64 Native Tools Command Prompt for Visual Studio to build.
+	ECHO Exiting...
+	EXIT
 )
 
 SET SOURCES_EXE=main.c
 
 SET INCLUDES=/I . 
-REM SET INCLUDES=%INCLUDES% /I../lib
-REM SET INCLUDES=%INCLUDES% /I"C:\Program Files (x86)\Windows Kits\8.1\Include\um"
-REM SET INCLUDES=%INCLUDES% /I"C:\Program Files (x86)\Windows Kits\8.1\Include\shared"
-REM SET INCLUDES=%INCLUDES% /I"C:\Program Files (x86)\Windows Kits\8.1\Include\winrt"
-REM SET INCLUDES=%INCLUDES% /I"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\include"
-REM SET INCLUDES=%INCLUDES% /I"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\atlmfc\include"
-REM SET INCLUDES=%INCLUDES% /I"C:\Program Files (x86)\Windows Kits\10\Include\10.0.10240.0\ucrt"
 
 SET DEFINES=/D"BUILD_DEBUG=1" /D"BUILD_ARCH64=1" /D"BUILD_WIN=1"
 
@@ -34,20 +24,12 @@ REM Oi - enable intrinsics
 REM Yc - specify precompiled header
 REM Gm - disable incremental build
 REM /MP - multithreaded compilation
-REM SET CFLAGS=/Z7 /W4 /sdl /MT /GR- /EHa /WX /nologo /Oi /Yc"precompiled.h" /Gm-
 SET CFLAGS=/Z7 /W4 /sdl /MT /GR- /EHa /WX /nologo /Oi /Gm- /MP
 
 SET LIBS="kernel32.lib" "user32.lib" "gdi32.lib" "d3d12.lib" "dxguid.lib" "dxgi.lib" "d3dcompiler.lib"
 
 REM opt:ref - strip unreferenced functions/data
-REM /SUBSYSTEM:windows,5.1 - run on Windows XP
-SET LFLAGS=/opt:ref /DEBUG /SUBSYSTEM:windows,5.1
-REM SET LFLAGS=%LFLAGS% /LIBPATH:../lib/
-REM SET LFLAGS=%LFLAGS% /LIBPATH:"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\lib"
-REM SET LFLAGS=%LFLAGS% /LIBPATH:"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\atlmfc\lib"
-REM SET LFLAGS=%LFLAGS% /LIBPATH:"C:\Program Files (x86)\Windows Kits\10\lib\10.0.10240.0\ucrt\x86"
-REM SET LFLAGS=%LFLAGS% /LIBPATH:"C:\Program Files (x86)\Windows Kits\8.1\lib\winv6.3\um\x86"
-REM SET LFLAGS=%LFLAGS% /LIBPATH:"C:\Program Files (x86)\Windows Kits\NETFXSDK\4.6.1\Lib\um\x86"
+SET LFLAGS=/opt:ref /DEBUG
 
 REM ////////////////////////////////////////////////
 REM Build exe
@@ -57,5 +39,4 @@ SET LFLAGS_EXE=
 
 ECHO.
 ECHO Building exe...
-cl %INCLUDES% %DEFINES% %DEFINES_EXE% %CFLAGS% %CFLAGS_EXE% %SOURCES_EXE% /link %LFLAGS% %LFLAGS_EXE% %LIBS% /OUT:demo.exe
-
+cl %INCLUDES% %DEFINES% %DEFINES_EXE% %CFLAGS% %CFLAGS_EXE% %SOURCES_EXE% /link %LFLAGS% %LFLAGS_EXE% %LIBS% /OUT:hello.exe
